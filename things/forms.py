6e6e14047165fc4b965e456d266c18ac79eebd2c
Ayesha Dorani <ayesha.dorani@kcl.ac.uk>
2023-12-03 00:05:33 +0000

    Create Thing form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
+from django import forms
+from django.contrib.auth import authenticate
+from .models import Thing
 
 # Create your forms here.
+# Exercise 1: Things form
+# The project includes a nearly empty file called things/forms.py. In this file, define a form called ThingForm, that contains the fields name, description, and quantity, but not created_at.
+
+# The form must accept valid inputs for Thing and reject invalid input for Thing.
+
+# The description field must be displayed as a Textarea. The quantity field must be displayed as NumberInput.
+
+class ThingForm(forms.ModelForm):
+    # description = forms.CharField(widget=forms.Textarea)
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50}),
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
+        }
+
+
--- things/views.py
+++ things/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'Post':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
